de3bdd52d3e76d53c20f220ef5f81ec167b0e26a
Georgette Webb <georgette.webb78@email.com>
2022-12-13 20:17:01 +1030

    intro

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ st.set_page_config(
 )
 
 st.title('Team 8 - Crime Statistics APP')
+st.markdown('In this Data Challenge Team 8 will be working with a SA Crime Dataset adapted from Crime Statistics 2021-22. Using this dataset, we aim to understand and explore the dataset in order to later investigate the different types of crimes and their differing characteristics to gain insights and solve problems to reduce the crime rate.The Members in Team 8 are: Giselle, Bruno, Luke, Georgette, Maryam1621 and Atousa')
+
 
 file_loaded = st.file_uploader('Upload you file here')
 
