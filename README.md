1987d08c3bba9c496b2b6e044a43da2be68f2e37
Georgette Webb <georgette.webb78@email.com>
2022-12-14 07:06:36 +1030

    edits corrections

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Team 8 Crime Statistics Data Challenge APP
 In this Data Challenge Team 8 will be working with a SA Crime Dataset adapted from Crime Statistics 2021-22. 
-Using this dataset. We aim to dempnstrate our newly acquired data science skills and use them ti inform how local governement or the South Australian Police (SAPOL) can improve the safety of their State. We aim to understand and explore the dataset in order to investigate the different types of crimes and their differing characteristics to gain insights and solve problems to reduce the crime rate.
+Using this dataset. We aim to demonstrate our newly acquired data science skills and use them to inform how Local Government, or the South Australian Police (SAPOL) can improve the safety of their State. We aim to understand and explore the dataset in order to investigate the different types of crimes and their differing characteristics to gain insights and solve problems to reduce the crime rate.
 
 The Members in Team 8 are: Giselle, Bruno, Luke, Georgette, Maryam1621 and Atousa
 
@@ -24,7 +24,7 @@ Zoom
 Teams
 Discord
 
-Languages and dependencys
+Languages and dependencies
 Python
 Pandas
 numpy
--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.set_page_config(
 )
 
 st.title('Team 8 Crime Statistics Data Challenge APP')
-st.markdown('In this Data Challenge Team 8 will be working with a SA Crime Dataset adapted from Crime Statistics 2021-22. Using this dataset. We aim to dempnstrate our newly acquired data science skills and use them ti inform how local governement or the South Australian Police (SAPOL) can improve the safety of their State. We aim to understand and explore the dataset in order to investigate the different types of crimes and their differing characteristics to gain insights and solve problems to reduce the crime rate.')
+st.markdown('In this Data Challenge Team 8 will be working with a SA Crime Dataset adapted from Crime Statistics 2021-22. Using this dataset. We aim to demonstrate our newly acquired data science skills and use them to inform how Local Government, or the South Australian Police (SAPOL) can improve the safety of their State. We aim to understand and explore the dataset in order to investigate the different types of crimes and their differing characteristics to gain insights and solve problems to reduce the crime rate.')
 st.markdown('The Members in Team 8 are: Giselle, Bruno, Luke, Georgette, Maryam1621 and Atousa')
 st.markdown('The Mentor for Team 8 is: Malgorzata')
 
