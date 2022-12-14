b857b40fd1bd131cea00f899e146a1925e01b84c
Georgette Webb <georgette.webb78@email.com>
2022-12-14 18:59:51 +1030

    cont

--- pages/slide.py
+++ pages/slide.py
@@ -1,9 +1,8 @@
 import streamlit as st
 
-df = st.session_state['df']
-
 st.title('Team 8 Slides')
 
+df = st.session_state['df']
 st.header('Description of the dataset uploaded')
 st.markdown('Description of the dataset uploaded')
 st.write(df.describe())
