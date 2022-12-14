74e0b9477c9ee1998811f2254b4315a386ae4784
Georgette Webb <georgette.webb78@email.com>
2022-12-14 20:09:12 +1030

    pic

--- pages/slide.py
+++ pages/slide.py
@@ -1,6 +1,4 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 
 st.set_page_config(
     page_title='Team 8 - Slides',
@@ -10,7 +8,7 @@ st.set_page_config(
 st.title('Team 8 Slides')
 
 from PIL import Image
-image = Image.open('Cover_Page.jpg')
+image = Image.open('Cover_Page.png')
 
 st.image(image, caption='Team 8 - Cover Page Slide')
 
