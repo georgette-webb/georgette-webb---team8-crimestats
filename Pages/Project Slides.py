06d73e1a9fc17e123316587835dd56561b03a995
Georgette Webb <georgette.webb78@email.com>
2022-12-14 16:29:20 +1030

    pages

--- Pages/Project Slides.py
+++ Pages/Project Slides.py
@@ -1,9 +1,6 @@
 import streamlit as st
 
-st.markdown("# Page 2 ❄️")
-st.sidebar.markdown("# Page 2 ❄️")
+st.markdown("# Page 2")
+st.sidebar.markdown("# Page 2")
 
-st.title('Team 8 - Crime Statisitcs')
-
-st.header('Description of the dataset uploaded')
-st.markdown('Description of the dataset uploaded')
+st.title('Team 8 - Crime Statisitcsvxcvx')
