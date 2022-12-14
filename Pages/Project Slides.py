2cc81e35e72352c7b3821b389ce60de7b41fd6e1
Georgette Webb <georgette.webb78@email.com>
2022-12-14 16:26:43 +1030

    check1

--- Pages/Project Slides.py
+++ Pages/Project Slides.py
@@ -5,7 +5,5 @@ st.sidebar.markdown("# Page 2 ❄️")
 
 st.title('Team 8 - Crime Statisitcs')
 
-st.title('Team 8 - Crime Statisitcs')
-st.title('Team 8 - Crime Statisitcs')
-st.title('Team 8 - Crime Statisitcs')
-st.title('Team 8 - Crime Statisitcs')
+st.header('Description of the dataset uploaded')
+st.markdown('Description of the dataset uploaded')
