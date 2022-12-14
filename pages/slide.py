95944ffaf379615dbb555e997dc558ed21c185ed
Georgette Webb <georgette.webb78@email.com>
2022-12-14 19:04:56 +1030

    slides

--- pages/slide.py
+++ pages/slide.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title='Team 8 - slides',
+    page_icon=':bar_chart:',
+    layout="wide"
+)
 st.title('Team 8 Slides')
 
 df = st.session_state['df']
