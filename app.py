4f0345384aefb0d0ddfc6b2c8b88a07aa4c5f303
Georgette Webb <georgette.webb78@email.com>
2022-12-14 16:08:23 +1030

    pages

--- app.py
+++ app.py
@@ -8,11 +8,26 @@ st.set_page_config(
     page_icon=':bar_chart:',
     layout="wide"
 )
+def home():
 
-st.markdown("# Main page 🎈")
-st.sidebar.markdown("# Main page 🎈")
+    st.sidebar.markdown("# Home")
 
+def slides():
+    st.markdown("# Project Slides")
+    st.sidebar.markdown("# Project Slides")
 
+def page3():
+    st.markdown("# Page 3")
+    st.sidebar.markdown("# Page 3")
+
+page_names_to_funcs = {
+    "Home": home,
+    "Project Slides": slides,
+    "Page 3": page3,
+}
+
+selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+page_names_to_funcs[selected_page]()
 
 st.title('Team 8 Crime Statistics Data Challenge APP')
 st.markdown('In this Data Challenge Team 8 will be working with a SA Crime Dataset adapted from Crime Statistics 2021-22. Using this dataset. We aim to demonstrate our newly acquired data science skills and use them to inform how Local Government, or the South Australian Police (SAPOL) can improve the safety of their State. We aim to understand and explore the dataset in order to investigate the different types of crimes and their differing characteristics to gain insights and solve problems to reduce the crime rate.')
